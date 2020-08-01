dd62673d594920529c82cea7e22b54f18b952594
Anton Belyy <anton.belyy@gmail.com>
2020-07-31 20:21:37 -0400

    Add type- and variable name checking

--- kairos_blocks_common.js
+++ kairos_blocks_common.js
@@ -22,6 +22,8 @@
  */
 'use strict';
 
+var is_enabled_typecheck = true;
+
 goog.provide('Blockly.Constants.Kairos');
 
 goog.require('Blockly');
@@ -61,35 +63,113 @@ Blockly.Constants.Kairos.EVENTS_ONCHANGE_MIXIN = {
         if (!this.workspace.isDragging || this.workspace.isDragging()) {
             return;  // Don't change state at the start of a drag.
         }
-        if (this.isInFlyout && this.type.includes('kairos_event_') && this.type.includes('_part_')) {
-            var pvm = this.workspace.getPotentialVariableMap();
-            var all_used_var_models = this.workspace.getAllVariables();
-            // Build a var model map for all used vars
-            var used_vars_map = {};
-            var inv_used_vars_map = {};
-            all_used_var_models.forEach(function(used_var) {
-                used_vars_map[used_var.getId()] = used_var.name;
-                inv_used_vars_map[used_var.name] = true;
-            });
-            var children = this.getChildren(false);
-            children.forEach(function(child) {
-                if (child.type == "variables_get") {
+        if (this.type.includes('kairos_event_') && this.type.includes('_part_')) {
+            // Make variable names unique in flyouts
+            if (this.isInFlyout) {
+                var pvm = this.workspace.getPotentialVariableMap();
+                var all_used_var_models = this.workspace.getAllVariables();
+                // Build a var model map for all used vars
+                var used_vars_map = {};
+                var inv_used_vars_set = new Set();
+                all_used_var_models.forEach(function(used_var) {
+                    used_vars_map[used_var.getId()] = used_var.name;
+                    inv_used_vars_set.add(used_var.name);
+                });
+                var children = this.getChildren(false);
+                children.forEach(function(child) {
+                    console.assert(child.type === "variables_get");
                     var var_id = child.getFieldValue('VAR');
                     if (var_id in used_vars_map) {
                         var new_name = used_vars_map[var_id];
                         do {
                             new_name = Blockly.Constants.Kairos.EVENTS_ONCHANGE_MIXIN.incrName(new_name);
-                        } while (new_name in inv_used_vars_map);
+                        } while (inv_used_vars_set.has(new_name));
                         var new_var_model = pvm.createVariable(new_name).getId();
                         child.setFieldValue(new_var_model, 'VAR');
                     }
-                }
-            });
+                });
+            }
         }
-
-
     }
 };
 
-Blockly.Extensions.registerMixin('kairos_events_checkVarName',
+Blockly.Extensions.registerMixin('kairos_events_checkVarsNamesAndTypes',
     Blockly.Constants.Kairos.EVENTS_ONCHANGE_MIXIN);
+
+function slots2types(var_slots) {
+    var_slots = Array.from(var_slots);
+    var types = new Set();
+    for (var i = 0; i < var_slots.length; i++) {
+        var var_slot_str = var_slots[i];
+        var event_name_and_arg_id = var_slot_str.split("_arg_");
+        var event_name = event_name_and_arg_id[0], arg_id = event_name_and_arg_id[1];
+        var slot_types = new Set(events_args[event_name][arg_id][1]);
+        if (i === 0) {
+            types = slot_types;
+        } else {
+            types = new Set([...types].filter(i => slot_types.has(i)));
+        }
+    }
+    return types;
+}
+
+function typecheck(ws) {
+    var vars_slots = {};
+    var blocks = ws.getAllBlocks(false);
+    for (var i = 0; i < blocks.length; i++) {
+        var block = blocks[i];
+        if (block.type.includes('kairos_event_') && block.type.includes('_part_')) {
+            var event_name_and_part_id_str = block.type.substr('kairos_event_'.length);
+            var event_name_and_part_id = event_name_and_part_id_str.split('_part_');
+            var event_name = event_name_and_part_id[0], part_id = parseInt(event_name_and_part_id[1]);
+            var children = block.getChildren(true);
+            for (var part_offset = 0; part_offset < children.length; part_offset++) {
+                var child = children[part_offset];
+                console.assert(child.type === "variables_get");
+                var var_id = child.getFieldValue('VAR');
+                // compute arg_id
+                var arg_id = events_parts_args[event_name][part_id][part_offset];
+                if (!(var_id in vars_slots)) {
+                    vars_slots[var_id] = new Set();
+                }
+                var event_name_and_arg_id = event_name + "_arg_" + arg_id.toString();
+                vars_slots[var_id].add(event_name_and_arg_id);
+            }
+        }
+    }
+    var vars_types = {};
+    for (var var_id in vars_slots) {
+        vars_types[var_id] = slots2types(vars_slots[var_id]);
+    }
+
+    for (var i = 0; i < blocks.length; i++) {
+        var block = blocks[i];
+        if (block.type === "variables_get") {
+            var var_id = block.getFieldValue('VAR');
+            if (var_id in vars_types) {
+                if (vars_types[var_id].size === 0) {
+                    block.setEnabled(false);
+                    block.setTooltip('This variable has contradicting types from the slots it is used in.');
+                } else {
+                    block.setEnabled(true);
+                    block.setTooltip(Blockly.Msg['VARIABLES_GET_TOOLTIP']);
+                }
+            }
+        }
+    }
+
+    console.log(vars_types);
+}
+
+function setTypecheck(new_typecheck_value) {
+    is_enabled_typecheck = new_typecheck_value;
+    if (is_enabled_typecheck) {
+        typecheck(workspace);
+    }
+}
+
+function workspaceOnChangeListener(e) {
+    if (is_enabled_typecheck) {
+        typecheck(workspace);
+    }
+}
