539915f6dd9672b493e922e2c0ff5630e39b27b7
Anton Belyy <anton.belyy@gmail.com>
2021-04-13 17:55:24 +0300

    Add KAIROS to FrameNet mapping

---
+++ ontologies/kairos/kairos2framenet.js
@@ -0,0 +1,365 @@
+{
+    "Contact.Contact.Unspecified": {
+        "Communication": {
+            "Participant": "Communicator",
+            "Topic": "Topic",
+            "Place": "Place"
+        }
+    },
+    "ArtifactExistence.ManufactureAssemble.Unspecified": {
+        "Building": {
+            "ManufacturerAssembler": "Agent",
+            "Artifact": "Created_entity",
+            "Components": "Components",
+            "Instrument": "Instrument",
+            "Place": "Place"
+        }
+    },
+    "Movement.Transportation.Unspecified": {
+        "Motion": {
+            "PassengerArtifact": "Theme",
+            "Vehicle": "Carrier",
+            "Origin": "Source",
+            "Destination": "Goal"
+        },
+        "Bringing": {
+            "Transporter": "Agent",
+            "PassengerArtifact": "Theme",
+            "Vehicle": "Carrier",
+            "Origin": "Source",
+            "Destination": "Goal"
+        }
+    },
+    "Contact.Contact.Broadcast": {
+        "Contacting": {
+            "Communicator": "Communicator",
+            "Recipient": "Addressee",
+            "Instrument": "Medium",
+            "Topic": "Topic",
+            "Place": "Place"
+        }
+    },
+    "Transaction.ExchangeBuySell.Unspecified": {
+        "Commercial_transaction": {
+            "Giver": "Seller",
+            "Recipient": "Buyer",
+            "AcquiredEntity": "Goods",
+            "PaymentBarter": "Money"
+        }
+    },
+    "Cognitive.Inspection.SensoryObserve": {
+        "Inspecting": {
+            "Observer": "Inspector",
+            "ObservedEntity": "Ground",
+            "Instrument": "Instrument",
+            "Place": "Location_of_protagonist"
+        }
+    },
+    "Cognitive.IdentifyCategorize.Unspecified": {
+        "Categorization": {
+            "Identifier": "Cognizer",
+            "IdentifiedObject": "Item",
+            "IdentifiedRole": "Category"
+        }
+    },
+    "Conflict.Attack.Unspecified": {
+        "Attack": {
+            "Attacker": "Assailant",
+            "Target": "Victim",
+            "Instrument": "Weapon",
+            "Place": "Place"
+        }
+    },
+    "Justice.ChargeIndict.Unspecified": {
+        "Notification_of_charges": {
+            "Prosecutor": "Arraign_authority",
+            "Defendant": "Accused",
+            "JudgeCourt": "Arraign_authority",
+            "Crime": "Charges",
+            "Place": "Place"
+        }
+    },
+    "Personnel.StartPosition.Unspecified": {
+        "Hiring": {
+            "Employee": "Employee",
+            "PlaceOfEmployment": "Employer",
+            "Position": "Position",
+            "Place": "Place"
+        }
+    },
+    "Contact.Contact.Meet": {
+        "Meet_with": {
+            "Topic": "Topic",
+            "Place": "Place"
+        }
+    },
+    "Cognitive.TeachingTrainingLearning.Unspecified": {
+        "Education_teaching": {
+            "TeacherTrainer": "Teacher",
+            "FieldOfKnowledge": "Subject",
+            "Learner": "Student",
+            "Institution": "Institution",
+            "Place": "Place"
+        }
+    },
+    "Cognitive.Research.Unspecified": {
+        "Research": {
+            "Researcher": "Researcher",
+            "Subject": "Topic",
+            "Means": "Means",
+            "Place": "Place"
+        }
+    },
+    "Justice.ArrestJailDetain.Unspecified": {
+        "Arrest": {
+            "Jailer": "Authorities",
+            "Detainee": "Suspect",
+            "Crime": "Charges",
+            "Place": "Place"
+        }
+    },
+    "Transaction.Donation.Unspecified": {
+        "Giving": {
+            "Giver": "Donor",
+            "Recipient": "Recipient",
+            "ArtifactMoney": "Theme",
+            "Place": "Place"
+        }
+    },
+    "Conflict.Defeat.Unspecified": {
+        "Beat_opponent": {
+            "Victor": "Winner",
+            "Defeated": "Loser",
+            "ConflictOrElection": "Competition",
+            "Place": "Place"
+        }
+    },
+    "Control.ImpedeInterfereWith.Unspecified": {
+        "Hindering": {
+            "Impeder": "Hindrance",
+            "ImpededEvent": "Protagonist",
+            "Place": "Place"
+        }
+    },
+    "Life.Consume.Unspecified": {
+        "Ingestion": {
+            "ConsumingEntity": "Ingestor",
+            "ConsumedThing": "Ingestibles",
+            "Place": "Place"
+        }
+    },
+    "Movement.Transportation.Evacuation": {
+        "Escaping": {
+            "PassengerArtifact": "Escapee",
+            "Vehicle": "Vehicle",
+            "Origin": "Undesirable_location",
+            "Destination": "Goal"
+        }
+    },
+    "Contact.ThreatenCoerce.Unspecified": {
+        "Commitment": {
+            "Communicator": "Speaker",
+            "Recipient": "Addressee",
+            "Topic": "Topic",
+            "Place": "Place"
+        }
+    },
+    "Medical.Intervention.Unspecified": {
+        "Cure": {
+            "Treater": "Healer",
+            "Patient": "Patient",
+            "MedicalIssue": "Affliction",
+            "Instrument": "Treatment",
+            "Place": "Place"
+        }
+    },
+    "Life.Injure.Unspecified": {
+        "Cause_harm": {
+            "Victim": "Victim",
+            "Injurer": "Agent",
+            "Instrument": "Instrument",
+            "BodyPart": "Body_part",
+            "Place": "Place"
+        }
+    },
+    "Contact.RequestCommand.Unspecified": {
+        "Request": {
+            "Communicator": "Speaker",
+            "Recipient": "Addressee",
+            "Topic": "Topic"
+        }
+    },
+    "ArtifactExistence.DamageDestroyDisableDismantle.Dismantle": {
+        "Cause_to_fragment": {
+            "Dismantler": "Agent",
+            "Artifact": "Whole_patient",
+            "Instrument": "Instrument",
+            "Components": "Pieces",
+            "Place": "Place"
+        }
+    },
+    "Life.Die.Unspecified": {
+        "Death": {
+            "Victim": "Protagonist",
+            "Place": "Place"
+        }
+    },
+    "Contact.Prevarication.Unspecified": {
+        "Prevarication": {
+            "Communicator": "Speaker",
+            "Recipient": "Addressee",
+            "Topic": "Topic",
+            "Place": "Place"
+        }
+    },
+    "Life.Infect.Unspecified": {
+        "Infecting": {
+            "Victim": "Infected_entity",
+            "InfectingAgent": "Infection",
+            "Source": "Infection_cause",
+            "Place": "Place"
+        }
+    },
+    "Disaster.FireExplosion.Unspecified": {
+        "Explosion": {
+            "FireExplosionObject": "Victim",
+            "Instrument": "Explosive",
+            "Place": "Place"
+        },
+        "Fire_burning": {
+            "FireExplosionObject": "Fire",
+            "Instrument": "Fuel",
+            "Place": "Place"
+        }
+    },
+    "Justice.Sentence.Unspecified": {
+        "Sentencing": {
+            "JudgeCourt": "Court",
+            "Defendant": "Convict",
+            "Crime": "Offense",
+            "Sentence": "Sentence",
+            "Place": "Place"
+        }
+    },
+    "Justice.Convict.Unspecified": {
+        "Verdict": {
+            "JudgeCourt": "Judge",
+            "Defendant": "Defendant",
+            "Crime": "Charges",
+            "Place": "Place"
+        }
+    },
+    "Justice.Acquit.Unspecified": {
+        "Verdict": {
+            "JudgeCourt": "Judge",
+            "Defendant": "Defendant",
+            "Crime": "Charges",
+            "Place": "Place"
+        }
+    },
+    "Personnel.EndPosition.Unspecified": {
+        "Firing": {
+            "Employee": "Employee",
+            "PlaceOfEmployment": "Employer",
+            "Position": "Position",
+            "Place": "Place"
+        },
+        "Quitting": {
+            "Employee": "Employee",
+            "PlaceOfEmployment": "Employer",
+            "Position": "Position",
+            "Place": "Place"
+        },
+        "Employment_end": {
+            "Employee": "Employee",
+            "PlaceOfEmployment": "Employer",
+            "Position": "Position",
+            "Place": "Place"
+        }
+    },
+    "Conflict.Demonstrate.Unspecified": {
+        "Protest": {
+            "Demonstrator": "Protester",
+            "Topic": "Issue",
+            "Place": "Place"
+        }
+    },
+    "Justice.TrialHearing.Unspecified": {
+        "Trial": {
+            "Prosecutor": "Prosecution",
+            "Defendant": "Defendant",
+            "JudgeCourt": "Judge",
+            "Crime": "Charges",
+            "Place": "Place"
+        }
+    },
+    "Conflict.Attack.DetonateExplode": {
+        "Detonate_explosive": {
+            "Attacker": "Agent",
+            "Target": "Victim",
+            "Instrument": "Instrument",
+            "ExplosiveDevice": "Explosive",
+            "Place": "Place"
+        }
+    },
+    "ArtifactExistence.DamageDestroyDisableDismantle.Damage": {
+        "Damaging": {
+            "Damager": "Agent",
+            "Artifact": "Patient",
+            "Instrument": "Instrument",
+            "Place": "Place"
+        }
+    },
+    "Contact.Contact.Correspondence": {
+        "Contacting": {
+            "Participant": "Communicator",
+            "Instrument": "Medium",
+            "Topic": "Topic",
+            "Place": "Place"
+        }
+    },
+    "GenericCrime.GenericCrime.GenericCrime": {
+        "Committing_crime": {
+            "Perpetrator": "Perpetrator",
+            "Place": "Place"
+        }
+    },
+    "ArtifactExistence.DamageDestroyDisableDismantle.Destroy": {
+        "Destroying": {
+            "Destroyer": "Destroyer",
+            "Artifact": "Patient",
+            "Instrument": "Instrument",
+            "Place": "Place"
+        }
+    },
+    "Disaster.Crash.Unspecified": {
+        "Impact": {
+            "DriverPassenger": "Impactor",
+            "Vehicle": "Impactor",
+            "CrashObject": "Impactee",
+            "Place": "Place"
+        }
+    },
+    "Justice.InvestigateCrime.Unspecified": {
+        "Criminal_investigation": {
+            "Investigator": "Investigator",
+            "Defendant": "Suspect",
+            "Crime": "Incident"
+        }
+    },
+    "Life.Illness.Unspecified": {
+        "Medical_conditions": {
+            "Victim": "Patient",
+            "Disease": "Ailment",
+            "Place": "Place"
+        }
+    },
+    "ArtifactExistence.DamageDestroyDisableDismantle.DisableDefuse": {
+        "Render_nonfunctional": {
+            "Disabler": "Agent",
+            "Artifact": "Artifact",
+            "Instrument": "Cause",
+            "Place": "Place"
+        }
+    }
+}
